d2050d81d5e0661ff5be6cedac3e406cb30dacde
Nazla Lutfia Ramadhani <nazlaramadhani115@gmail.com>
2026-04-23 12:04:15 +0700

    update

---
+++ week4/src/Data/Dummy.js
@@ -0,0 +1,22 @@
+// Dummy user untuk login
+export const dummyUser = {
+  email: "admin@mail.com",
+  password: "123456",
+  name: "Admin",
+};
+
+// Dummy data mahasiswa
+export const mahasiswaList = [
+  {
+    nim: "202315259",
+    nama: "Nazla Lutfia Ramadhani",
+  },
+  {
+    nim: "202315263",
+    nama: "Sabrina Aska Amalina",
+  },
+  {
+    nim: "202315264",
+    nama: "Aurelia Dwi Wijayanti",
+  },
+];
